850b0de90e341891e33a0cffca2a8a10a2c41ae2
Artem Shimanchuk <shimart0606@mail.ru>
2025-08-24 13:56:15 +0300

    feat: add fonts to assets and include them to scss file

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,9 +1,12 @@
+import "../styles/main.scss"
 
 export function App() {
-  return (
-    <>
-
-    </>
-  )
+	return (
+		<>
+			<div style={{fontFamily: ''}}>Lorem ipsum</div>
+			<div>Artem</div>
+			<div>Hello</div>
+		</>
+	)
 }
 
